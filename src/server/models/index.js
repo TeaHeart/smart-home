23a8940c34474a667b01084221133d9fdc0393f3
TeaHeart <1069696634@qq.com>
2025-08-24 11:47:40 +0800

    chore: 删除冗余测试代码

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -11,88 +11,15 @@ export { User, userRoleList, Device, Message, messageTypeList, Rule, Log, logLev
 export default async function setupMongo() {
   await mongoose.connect(process.env.MONGO_URL)
 
-  if ((await User.countDocuments()) === 0) {
-    const user = await User.create({
+  const user = await User.findOne({ username: 'sa' })
+
+  if (!user) {
+    const adminUser = await User.create({
       role: 'admin',
       username: 'sa',
       password: 'sa',
     })
-    console.log('default admin created', user)
-  }
-
-  const user = await User.findOne({ username: 'sa' })
-
-  if ((await Device.countDocuments()) === 0) {
-    const device = await Device.create({
-      deviceId: '00000000000000000000000000000000',
-      online: false,
-      model: {
-        name: 'test device',
-        properties: {
-          property1: {
-            type: 'boolean',
-          },
-        },
-        services: {
-          service1: {
-            param1: {
-              type: 'number',
-            },
-          },
-        },
-        events: {
-          event1: {
-            param1: {
-              type: 'number',
-            },
-          },
-        },
-      },
-      description: 'test device',
-    })
-    console.log('default device created', device)
-  }
-
-  const device = await Device.findOne({ deviceId: '00000000000000000000000000000000' })
-
-  if ((await Message.countDocuments()) === 0) {
-    const message = await Message.create({
-      type: 'offline',
-      device: device.id,
-      time: Date.now(),
-      data: device.model,
-    })
-    console.log('default message created', message)
-  }
-
-  if ((await Rule.countDocuments()) === 0) {
-    const rule = await Rule.create({
-      name: 'test rule',
-      enabled: false,
-      source: device.id,
-      condition: 'property1 == 0',
-      target: device.id,
-      service: {
-        service1: {
-          param1: 1,
-        },
-      },
-      description: 'test rule',
-    })
-    console.log('default rule created', rule)
-  }
-
-  if ((await Log.countDocuments()) === 0) {
-    const log = await Log.create({
-      level: 'info',
-      user: user.id,
-      time: Date.now(),
-      operation: 'register',
-      data: {
-        test: user.toJSON(),
-      },
-    })
-    console.log('default log created', log)
+    console.log('default admin created', adminUser)
   }
 
   console.log('mongo is connected', process.env.MONGO_URL)
